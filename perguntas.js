02194f67cf03d42f2594cfcb3f8dec1436e74278
kaua-daVaLL <00001125624978sp@al.educacao.sp.gov.br>
2024-11-26 14:26:10 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Física',
-    'Quem foi Marrie Currie?',
-    'Marie Curie (1867–1934) foi uma cientista polonesa-francesa pioneira no campo da radioatividade. Ela foi uma das figuras mais influentes da ciência moderna e a primeira mulher a ganhar o Prêmio Nobel, além de ser a única pessoa a receber o prêmio em duas áreas diferentes: Física e Química.'
+    'Quem descobril os conceitos da radioatividade?',
+    'Foi o casal de cientístas Marrie e Pierre currie que, descobriram a radioatividade em cima dos conceitos iniciais feito por Henri Becquerel'
 )
 
 criaCartao(
