d5c9c230be6e9e302caa2a7a26b3e0fbdf83b68a
kaua-daVaLL <00001125624978sp@al.educacao.sp.gov.br>
2024-11-26 14:20:49 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,6 +1,6 @@
 criaCartao(
     'Física',
-    'O que foi Marrie Currie?',
+    'Quem foi Marrie Currie?',
     'Marie Curie (1867–1934) foi uma cientista polonesa-francesa pioneira no campo da radioatividade. Ela foi uma das figuras mais influentes da ciência moderna e a primeira mulher a ganhar o Prêmio Nobel, além de ser a única pessoa a receber o prêmio em duas áreas diferentes: Física e Química.'
 )
 
