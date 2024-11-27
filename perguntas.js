8e8f201a212db7186d50f5558aee479fd8510795
kaua-daVaLL <00001125624978sp@al.educacao.sp.gov.br>
2024-11-27 07:50:47 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -7,17 +7,17 @@ criaCartao(
 criaCartao(
     'Física',
     'Quem foi físico(a) responsável pela criação dos motores movidos a corrente elétrica alternada?',
-    'O físico responsável pela criação dos motores movidos a corrente elétrica foi nikola Tesla'
+    'Nikola Tesla, o grande físico que apresentou, de forma popular, a corrente elétrica alternativa'
 )
 
 criaCartao(
     'Filosofia',
-    'Qual o conceito de: Barbárie?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Quais os riscos do senso crítico do ser humano?',
+    'O senso crítico da ao ser humano a capacidade de pensar e sua própria opinião, e, com isso, podendo descrever o que é "certo" ou "errado".'
 )
 
 criaCartao(
     'Questão anônima interpretativa',
     'Por que o racismo ainda se faz presente na sociedade contemporânea?',
-    'O racismo é um contexto que aborda os idéias e a ética de um ser consciente. Racismo é um sinônimo para: "humanidade".'
+    'O racismo é um contexto que aborda os ideais e a ética de um ser racional-inteligente. Racismo é um sinônimo para humanidade.'
 )
